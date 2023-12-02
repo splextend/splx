345ce41a48ced933fe9c640add0dca2f1a967d53
undercloud <lodashes@gmail.com>
2023-12-02 16:21:23 +0400

    Add files via upload

--- Splx/Stream/StreamFilter.php
+++ Splx/Stream/StreamFilter.php
@@ -6,14 +6,13 @@ use php_user_filter;
 use Splx\IO\Stream;
 
 /**
- * Class StreamContext
+ * Class StreamFilter
  *
  * @category PHP Standard Library Extension
  * @package  Splx
  * @license  https://opensource.org/licenses/MIT MIT
  * @link     http://github.com/splextend/splextend
  */
-
 abstract class StreamFilter extends php_user_filter
 {
     public function onCreate()
@@ -26,20 +25,41 @@ abstract class StreamFilter extends php_user_filter
 
     }
 
+    /**
+     * @param $in
+     * @param $out
+     * @param $consumed
+     * @param $closing
+     * @return int
+     */
     public function filter($in, $out, &$consumed, $closing)
     {
-        $this->handle(
-            Stream::createFromResource($in),
-            Stream::createFromResource($out),
+        try {
+            $response = $this->handle(
+                Stream::createFromResource($in),
+                Stream::createFromResource($out),
+                $consumed,
+                $closing
+            );
 
-            $closing
-        );
+            if (false === $response) {
+                return PSFS_FEED_ME;
+            }
+
+            return PSFS_PASS_ON;
+        } catch (Exception $e) {
+            return PSFS_ERR_FATAL;
+        } catch (Throwable $e) {
+            return PSFS_ERR_FATAL;
+        }
     }
 
-    public function getFilterName()
-    {
-        return $this->filtername;
-    }
-
-    abstract function handle(Stream $inputStream, Stream $outputStream);
+    /**
+     * @param Stream $inputStream
+     * @param Stream $outputStream
+     * @param $consumed
+     * @param $isEnd
+     * @return mixed
+     */
+    abstract function handle(Stream $inputStream, Stream $outputStream, &$consumed, $isEnd);
 }
